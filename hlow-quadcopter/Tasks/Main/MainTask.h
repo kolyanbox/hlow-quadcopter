50fca473ea1a9175a834a37917b4e7d694434cb2
lukkezeh <lukkezeh@gmail.com>
2012-07-13 20:02:54 +0000

    Loging logs one message. After one message it stops logging.

--- hlow-quadcopter/Tasks/Main/MainTask.h
+++ hlow-quadcopter/Tasks/Main/MainTask.h
@@ -2,15 +2,6 @@
 #define MAINTASK_H
 
 #include <lpc_types.h>
-#include <Interfaces/Actuators/Actuators.h>
-#include <CoOS.h>
-
-#include <Tasks/Heartbeat/Heartbeat.h>
-#include <Tasks/Distancetoground/DistanceToGroundTask.h>
-#include <Tasks/Angle/AngleTask.h>
-#include <Tasks/Temperature/Temperature.h>
-#include <Tasks/Pressure/PressureTask.h>
-#include <Tasks/Debug/DebugTask.h>
 
 void MainTask (void* pdata);
 void setOsTime (int osTime);
