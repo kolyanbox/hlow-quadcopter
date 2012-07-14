d7259bee7271f86585244a1f54d6e352567bc738
lukkezeh <lukkezeh@gmail.com>
2012-07-14 16:39:40 +0000

    Solved issue with printing a 3 before distance to ground

--- hlow-quadcopter/Tasks/Distancetoground/DistanceToGroundTask.c
+++ hlow-quadcopter/Tasks/Distancetoground/DistanceToGroundTask.c
@@ -1,11 +1,11 @@
 #include <Tasks/Distancetoground/DistanceToGroundTask.h>
 #include <General/util.h>
+char c[30];
 void DistanceToGroundTask (void* pdata)
 {
 	for(;;)
 	{
 		int distanceToGround = getCurrentHeightInCm();
-		char c[30];
 		Itoa(distanceToGround,c,10);
 		WriteDebugInformation(c,DistanceToGround);
 		CoTimeDelay(0,0,1,0);
